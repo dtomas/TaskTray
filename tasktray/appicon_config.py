32f7c31a4edf72131f0a3fbabe799a4bf3cd9a5e
Dennis Tomas <dtomas@onegreenbyte.com>
2016-02-16 23:00:40 +0100

    Add doc string and default value to AppIconConfig.themed_icons.

--- tasktray/appicon_config.py
+++ tasktray/appicon_config.py
@@ -2,4 +2,7 @@ from traylib.config import Config, Attribute
 
 
 class AppIconConfig(Config):
-    themed_icons = Attribute()
+    themed_icons = Attribute(default=False)
+    """
+    C{True} if TaskTray should try to use icons from themes for L{AppIcon}s.
+    """
