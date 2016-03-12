2d5ad41310b16cde1f622f8455edb9c08a9e5c8b
Dennis Tomas <dtomas@onegreenbyte.com>
2016-03-12 11:40:29 +0100

    Create box in manage() and disconnect handlers in unmanage().

--- tasktray/appitem_manager.py
+++ tasktray/appitem_manager.py
@@ -13,11 +13,11 @@ from tasktray.desktop_app import DesktopApp
 def manage_appitems(tray, screen, icon_config, win_config, get_app_by_path,
                     get_app_by_name):
 
-    box = ItemBox("appitems")
-    tray.add_box(box)
-
     class state:
         initializing = True
+        box = None
+        screen_handlers = []
+        box_handlers = []
 
     def get_pinned_items_path():
         return os.path.join(
@@ -26,14 +26,14 @@ def manage_appitems(tray, screen, icon_config, win_config, get_app_by_path,
 
     def save_pinned_items(*args):
         pinned_items = []
-        for item in box.items:
+        for item in state.box.items:
             if item.app is not None and item.is_pinned:
                 pinned_items.append(item.app.path)
         with open(get_pinned_items_path(), "w") as f:
             json.dump(pinned_items, f)
 
     def window_opened(screen, window):
-        for item in box.items:
+        for item in state.box.items:
             if item.offer_window(window):
                 return
         if window.get_class_group() is None:
@@ -45,13 +45,11 @@ def manage_appitems(tray, screen, icon_config, win_config, get_app_by_path,
         appitem.connect("pinned", save_pinned_items)
         appitem.connect("unpinned", save_pinned_items)
         appitem.add_window(window)
-        box.add_item(appitem)
-
-    screen_handlers = [
-        screen.connect("window-opened", window_opened),
-    ]
+        state.box.add_item(appitem)
 
     def manage():
+        state.box = ItemBox("appitems")
+        tray.add_box(state.box)
         pinned_items_path = get_pinned_items_path()
         if os.path.exists(pinned_items_path):
             with open(pinned_items_path, "r") as f:
@@ -69,23 +67,35 @@ def manage_appitems(tray, screen, icon_config, win_config, get_app_by_path,
                     app=app,
                     pinned=True,
                 )
-                box.add_item(appitem)
+                state.box.add_item(appitem)
         for window in screen.get_windows():
             window_opened(screen, window)
             yield None
+        state.screen_handlers = [
+            screen.connect("window-opened", window_opened),
+        ]
+        state.box_handlers = [
+            state.box.connect("item-added", item_added),
+            state.box.connect("item-reordered", save_pinned_items),
+        ]
         state.initializing = False
 
     def unmanage():
-        box.destroy()
-        yield None
+        for handler in state.screen_handlers:
+            screen.disconnect(handler)
+            yield None
+        for handler in state.box_handlers:
+            state.box.disconnect(handler)
+            yield None
+        state.box.destroy()
+        state.box = None
+        state.screen_handlers = []
+        state.box_handlers = []
 
     def item_added(box, item):
         if not state.initializing:
             save_pinned_items(item)
         item.connect("pinned", save_pinned_items)
         item.connect("unpinned", save_pinned_items)
-
-    box.connect("item-added", item_added)
-    box.connect("item-reordered", save_pinned_items)
 
     return manage, unmanage
